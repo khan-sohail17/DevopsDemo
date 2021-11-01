3987af2d79b29dfad42d460d00d859d9cc03ac05
khan-sohail17 <sohil.6218075.it@mhssce.ac.in>
2021-11-01 09:39:28 +0530

    First commit

--- DevopsDemo/Program.cs
+++ DevopsDemo/Program.cs
@@ -7,6 +7,7 @@ namespace DevopsDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Commit");
         }
     }
 }
